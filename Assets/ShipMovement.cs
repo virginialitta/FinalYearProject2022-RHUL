8e7b2762c0ec5d38359e8853c6d5f9aeff2be8e3
Virginia Litta Modignani <zjac135@live.rhul.ac.uk>
2022-11-14 21:17:25 +0000

    Added first sprite and implemented X axis movement

---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    float movementX;
+    public float movementSpeed = 5f;
+    Rigidbody2D rb; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementX = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(movementSpeed * movementX, rb.velocity.y);
+        rb.velocity = movement;
+    }
+}
